8e6b59fc508528a5aa1f4e42cffac816cacf98ca
GMNDS <gabriel@gmnds.com>
2024-11-06 13:12:50 -0300

    Cria Classe de conexão com banco de dados

---
+++ infra/database.js
@@ -0,0 +1,46 @@
+import { Client } from "pg";
+
+export class Database {
+	constructor() {
+		this.client = new Client({
+			user: process.env.POSTGRES_USER,
+			port: process.env.POSTGRES_PORT,
+			host: process.env.POSTGRES_HOST,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+		});
+		this.connected = false;
+	}
+
+	async connect() {
+		if (!this.connected) {
+			await this.client.connect();
+			this.connected = true;
+		}
+	}
+
+	async disconnect() {
+		if (this.connected) {
+			await this.client.end();
+			this.connected = false;
+		}
+	}
+
+	async query(query, params = []) {
+		try {
+			await this.connect();
+			const { rows } = await this.client.query(query, params);
+			return rows;
+		} catch (error) {
+			const formattedQuery = query.replace(
+				/\$(\d+)/g,
+				(_, index) => params[index - 1],
+			);
+			console.log(params);
+			console.error(`Erro ao executar a query ${formattedQuery}`);
+			throw error;
+		} finally {
+			this.disconnect();
+		}
+	}
+}
